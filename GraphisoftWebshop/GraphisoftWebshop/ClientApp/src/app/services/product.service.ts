d40cf78177308bad176b4cb6528ad13794ddc3c1
Dévényi Lóránt <d.lorant92@gmail.com>
2018-04-04 20:52:23 +0200

    Delete unnessesery console logs.

--- GraphisoftWebshop/GraphisoftWebshop/ClientApp/src/app/services/product.service.ts
+++ GraphisoftWebshop/GraphisoftWebshop/ClientApp/src/app/services/product.service.ts
@@ -26,9 +26,6 @@ export class ProductService {
 
     constructor(private networkService: NetworkService, protected http: HttpClient) { }
 
-
-  //  private baseUrl: string = environment.API_URL;
-
     public baseUrl(): string {
         let base = '';
 
@@ -46,58 +43,18 @@ export class ProductService {
         let params = new HttpParams();
 
         let requestBody = params.toString();
-        console.log("aaaa " + this.baseUrl() + this.url);
         
-        //return this.http.post<Product[]>(this.baseUrl() + this.url, requestBody, { headers: header });
         return this.http.get<Product[]>(this.baseUrl() + this.url);
 
     }
 
     delete(product: Product): Observable<Product> {
-        console.log("delete");
-        console.log("bbb " + this.baseUrl() + this.url + '/delete/' + product.id);
-
         return this.http.delete<Product>(this.baseUrl() + this.url + '/delete/' + product.id);
     }
 
     create(product: Product): Observable<Product> {
-        console.log("create");
-        console.log("ccc " + this.baseUrl() + this.url + '/create');
-        console.log(JSON.stringify(product));
-
         return this.http.post<Product>(this.baseUrl() + this.url + '/create', JSON.stringify(product), this.getRequestHeaders());
     }
-
- //   list(): Observable<Product[]> {
-        // return this.http.get(this.url).map(this.http.extractJson);
-
-     //   return this.networkService.get<ApiResponse<Product[]>>(this.url).pipe(
-     //       tap(_ => console.log("get payload")),
-     //       map(response => response.payload),
-     //       catchError(this.handleError<Product[]>('getProducts', null))
-  //      ); 
- //   }
-
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     
-    private handleError<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-            // TODO: send the error to remote logging infrastructure
-            console.error(error); // log to console instead
-
-            // TODO: better job of transforming error for user consumption
-            console.log(`${operation} failed: ${error.message}`);
-
-            // Let the app keep running by returning an empty result.
-            return of(result as T);
-        };
-    }
-    */
-
 
     protected getRequestHeaders(): { headers: HttpHeaders | { [header: string]: string | string[]; } } {
         let headers = new HttpHeaders({
